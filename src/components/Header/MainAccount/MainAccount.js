7825998db5943d0320c353af5c2f08f25a21ac98
BONOLIFE <bonolife_@naver.com>
2018-11-10 20:09:21 +0900

    업데이트 : )

---
+++ src/components/Header/MainAccount/MainAccount.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './MainAccount.scss'
+
+const MainAccount = () => {
+    return (
+        <div className="MainAccount">
+            <ul>
+                <li>로그인</li>
+                <li>회원가입</li>
+                <li>마이페이지</li>
+            </ul>
+        </div>
+    );
+  };
+  
+  export default MainAccount;
