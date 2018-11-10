007c17c413edc2240b8ecc172b5878945af127e0
gtg7784 <gtg7784@lab-c.me>
2018-11-11 08:07:40 +0900

    ㅁㄴㅇㄹ

---
+++ src/components/Headers/Banner/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Banner.scss';
+
+import BannerImg from '../../../static/images/main_banner.jpg'
+
+const Banner = () => {
+    return (
+        <div className="Banner">
+            <div>
+                <li>introduce</li>
+                <li>lecture</li>
+                <li>study</li>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
-
+import Logos from '../components/Headers/Logo/Logo'
+import Login from '../components/Headers/Login/Login'
+import Banner from '../components/Headers/Banner/Banner.js'
 import { Route, Switch } from 'react-router-dom';
-import Home from '../pages/Home.js';
+
+import Home from './Home/Home'
+import Signin from './Signin/Signin'
+import Signup from './Signup/Signup'
+import Mypage from './Mypage/Mypage'
 
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="Main">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            </Switch>
+      <div className="App">
+        <Logos />
+        <Login />
+        <nav>
+            <Banner />
+        </nav>
+          <main>
+              <div>
+                  <Switch>
+                      <Route path="/" exact component={Home} />
+                      <Route path="/signin" exact component={Signin} />
+                      <Route path="/signup" exact component={Signup} />
+                      <Route path="/mypage" exact component={Mypage} />
+                  </Switch>
+              </div>
+      </main>
       </div>
     );
   }
