272cf57d7b8c86f3ef06587f761033db95195f3f
BONOLIFE <bonolife_@naver.com>
2018-11-10 22:12:22 +0900

    이상한 업데이트

---
+++ src/components/Header/MainBanner/MainBanner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './MainBanner.scss';
+
+const MainBanner = () => {
+    return (
+        <div className="MainBanner"></div>
+    );
+  };
+  
+  export default MainBanner;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import MainHeader from '../components/Header/MainHeader/';
 import MainAccount from '../components/Header/MainAccount/';
+import MainBanner from '../components/Header/MainBanner/';
 
 const Home = () => {
     return (
         <div className="Header">
             <MainHeader/>
             <MainAccount/>
+            <MainBanner/>
         </div>
     );
 };
